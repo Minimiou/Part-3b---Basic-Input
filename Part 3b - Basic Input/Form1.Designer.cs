86eeef8b30b4204e057765d62df56cf6b051f82d
Aya Kh <akhalife2003@gmail.com>
2020-09-23 21:18:00 -0400

    Add project files.

---
+++ Part 3b - Basic Input/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Part_3b___Basic_Input
+{
+    partial class BasicInput
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInstruction = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.nudAge = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAge)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblInstruction
+            // 
+            this.lblInstruction.BackColor = System.Drawing.Color.Snow;
+            this.lblInstruction.Font = new System.Drawing.Font("Salina", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstruction.ForeColor = System.Drawing.Color.Gold;
+            this.lblInstruction.Location = new System.Drawing.Point(23, 12);
+            this.lblInstruction.Name = "lblInstruction";
+            this.lblInstruction.Size = new System.Drawing.Size(221, 75);
+            this.lblInstruction.TabIndex = 1;
+            this.lblInstruction.Text = "Please fill in the missing information:";
+            this.lblInstruction.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Minion Pro Med", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblName.Location = new System.Drawing.Point(21, 95);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(89, 22);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Your name:";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Font = new System.Drawing.Font("Minion Pro Med", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAge.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblAge.Location = new System.Drawing.Point(21, 124);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(74, 22);
+            this.lblAge.TabIndex = 3;
+            this.lblAge.Text = "Your age:";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Font = new System.Drawing.Font("Minion Pro Med", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeight.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblHeight.Location = new System.Drawing.Point(21, 153);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(124, 22);
+            this.lblHeight.TabIndex = 4;
+            this.lblHeight.Text = "Your height (m):";
+            // 
+            // txtName
+            // 
+            this.txtName.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.txtName.Font = new System.Drawing.Font("Minion Pro", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.ForeColor = System.Drawing.Color.DarkRed;
+            this.txtName.Location = new System.Drawing.Point(144, 96);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 25);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.txtHeight.Font = new System.Drawing.Font("Minion Pro", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHeight.ForeColor = System.Drawing.Color.DarkRed;
+            this.txtHeight.Location = new System.Drawing.Point(144, 154);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 25);
+            this.txtHeight.TabIndex = 6;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.Font = new System.Drawing.Font("Salina", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.ForeColor = System.Drawing.Color.LightCoral;
+            this.lblOutput.Location = new System.Drawing.Point(30, 240);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(214, 170);
+            this.lblOutput.TabIndex = 9;
+            this.lblOutput.Text = "???";
+            this.lblOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.Gold;
+            this.btnSubmit.FlatAppearance.BorderColor = System.Drawing.Color.DarkGoldenrod;
+            this.btnSubmit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Maroon;
+            this.btnSubmit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkGoldenrod;
+            this.btnSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSubmit.Font = new System.Drawing.Font("Minion Pro Med", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnSubmit.Location = new System.Drawing.Point(27, 187);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(217, 49);
+            this.btnSubmit.TabIndex = 10;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // nudAge
+            // 
+            this.nudAge.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.nudAge.Font = new System.Drawing.Font("Minion Pro", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudAge.ForeColor = System.Drawing.Color.DarkRed;
+            this.nudAge.Location = new System.Drawing.Point(144, 125);
+            this.nudAge.Name = "nudAge";
+            this.nudAge.Size = new System.Drawing.Size(100, 25);
+            this.nudAge.TabIndex = 11;
+            // 
+            // BasicInput
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(275, 419);
+            this.Controls.Add(this.nudAge);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblInstruction);
+            this.Name = "BasicInput";
+            this.Text = "Future";
+            ((System.ComponentModel.ISupportInitialize)(this.nudAge)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblInstruction;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.NumericUpDown nudAge;
+    }
+}
+
---
+++ Part 3b - Basic Input/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Part_3b___Basic_Input
+{
+    public partial class BasicInput : Form
+    {
+        string name;
+        int age;
+        double height;
+
+
+        public BasicInput()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            {
+                name = txtName.Text;
+
+                age = Convert.ToInt32(nudAge.Value);
+                age = 82 - age;
+
+                height = Convert.ToDouble(txtHeight.Text);
+                height = 2.72 - height;
+                height = Math.Round(height, 2);
+
+                lblOutput.Text = $"Hello {name}! You are {age} years below life expectancy in Canada and are {height}m shorter than the tallest person on earth. ";
+
+            }
+        }
+    }
+}
